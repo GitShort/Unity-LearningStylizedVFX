51b631343f9aeddc06c0180486a21045f45cca53
Airidas <airidas12111@gmail.com>
2022-08-12 13:40:14 +0300

    Arrow dissolve and impact

--- Assets/Main/Scripts/Arrow.cs
+++ Assets/Main/Scripts/Arrow.cs
@@ -1,19 +1,47 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.VFX;
 
 public class Arrow : MonoBehaviour
 {
     Rigidbody rb;
+    [SerializeField] VisualEffect arrowVFX;
+    [SerializeField] VisualEffect arrowDissolve;
+    [SerializeField] VisualEffect hitWallVFX;
+    bool isWallHit = false;
+
+    Animator anim;
 
     private void Start()
     {
         rb = GetComponent<Rigidbody>();
+        anim = GetComponent<Animator>();
     }
 
     private void OnCollisionEnter(Collision collision)
     {
-        Debug.Log("AA");
-        rb.constraints = RigidbodyConstraints.FreezeAll;
+        if(!isWallHit)
+        {
+            isWallHit = true;
+            rb.constraints = RigidbodyConstraints.FreezeAll;
+            if (hitWallVFX != null)
+                hitWallVFX.Play();
+            DestroyArrow();
+        }
+    }
+
+    void DestroyArrow()
+    {
+        StartCoroutine(DissolveArrow());
+        Destroy(this.gameObject, 15f);
+    }
+
+    IEnumerator DissolveArrow()
+    {
+        yield return new WaitForSeconds(2f);
+        anim.SetTrigger("DissolveArrow");
+        arrowDissolve.Play();
+        arrowVFX.enabled = false;
     }
 }
